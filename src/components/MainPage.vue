9a9a6bb29b5eaf178001e636561ed4a25ceeaca4
Minani <minanihertierluc@gmail.com>
2022-01-09 23:57:18 -0500

    Added Wallpaper Image

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -8,6 +8,7 @@
             src="https://www.fillmurray.com/1920/1080"
             alt=""
           /> -->
+          
           <div class="hero-body container has-text-centered">
             <div>
               <h1 class="title" style="font-size: 60px; margin-bottom: 10px">
@@ -112,7 +113,6 @@
                 </div>
               </div>
 
-
               <div class="column is-4 modal-button" data-target="modal-image">
                 <div class="card is-shady">
                   <div class="card-image">
@@ -199,7 +199,7 @@
             </div>
           </section>
         </div>
-        
+
         <div id="Property">
           <section class="hero is-white is-fullheight">
             <div class="hero-body">
@@ -296,7 +296,6 @@
                     data-aos="fade-right"
                     class="column is-10-mobile is-offset-1-mobile is-10-tablet is-offset-1-tablet is-4-desktop is-offset-1-desktop is-4-widescreen is-offset-1-widescreen is-4-fullhd is-offset-1-fullhd"
                   >
-                  
                     <figure>
                       <img src="@/assets/img/undraw_refreshing.svg" />
                     </figure>

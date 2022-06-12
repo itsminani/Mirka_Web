067d680ad1d173da167542d84802b5580f816c99
Minani <minanihertierluc@gmail.com>
2022-06-12 05:51:16 -0400

    Added People object to keep track of roles and names of each person making up the team

---
+++ src/components/people.vue
@@ -0,0 +1,30 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+data() {
+    return {
+        people: [
+            {role: "Chair Person",
+            name: "BUZAYIRE Angele",
+            expertise: "a professional lawyer"},
+            {role: "Vice Chair Person",
+            name: "BUZAYIRE Angele",
+            expertise: "a professional in finance"},
+            {role: "Member",
+            name: "RUKANGIRA Emmanuel",
+            expertise: "a professional Lawyer"},
+            {role: "Member",
+            name: "UWAMUNGU Kalisa Antoine",
+            expertise: "an insurance professional and executive"},
+            {role: "Member",
+            name: "MUSONERA Santos",
+            expertise: "A business person"},
+            {role: "Managing Director",
+            name: "MINANI Gregoire",
+            expertise: "an insurance professional"}
+        ]
+    }
+},
+</script>
